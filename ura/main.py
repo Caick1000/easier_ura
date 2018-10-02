75bddcbdc4e9ad36dbff21917a9f29ccb056e472
DESKTOP-U4G76IE\Caick <caick.passarella@talkeen.com.br>
2018-10-02 16:33:39 -0300

    better code + choice for file name

--- ura/main.py
+++ ura/main.py
@@ -1,3 +1,4 @@
+# KIVY imports
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -5,67 +6,55 @@ from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 from kivy.core.window import Window
 from kivy.uix.anchorlayout import AnchorLayout
-Window.clearcolor = (0, 0.05, 0.1, 0)
 
+# Misc imports
 from modules import header, body, footer
 from datetime import datetime
 
 current_date = datetime.now().strftime("%Y/%m/%d")
 
-def validations(gender, context, path, custom_path, debug):
-    if gender == 'Female':
-        gender = 'f'
 
-    elif gender == 'Male':
-        gender = 'm'
-        
-    else: gender = 'f'
+# Function to check if a parameter is wrong or empty
+def validations(gender, context, path, custom_path, debug, ip):
+    gender = 'f' if gender == 'Female' else 'm' if gender == 'Male' else 'f'
+    context = 'missing_context' if context == '' else context
+    path = 'global/{}'.format(gender) if path == '' else path
+    custom_path = path if custom_path == '' else custom_path
+    debug = '0' if debug == False else '1'
+    ip = 'missing_ip' if ip == '' else ip
 
-    if context == '':
-        context = 'no_name'
+    if custom_path[:-1] == '/' or custom_path[:-1] == '\\':
+        custom_path = custom_path[:-1]
 
-    if path == '':
-        path = 'global/{}'.format(gender)
+    if custom_path[0:] == '/' or custom_path[0:] == '\\':
+        custom_path = custom_path[0:]
 
-    if custom_path == '':
-        custom_path = path
+    if path[:-1] == '/' or path[:-1] == '\\':
+        path = path[:-1]
 
-    if debug == False:
-        debug = '0'
-    else:
-        debug = '1'
+    if path[0:] == '/' or path[0:] == '\\':
+        path = path[0:]
 
-    return(gender, context, path, custom_path, debug)
+    return(gender, context, path, custom_path, debug, ip)
+
 
 class CreateUra(GridLayout):
 
     def uraConfig(self):
         debug = self.ids.check_debug.active
         gender = self.ids.gender.text
-        #ura_file_name = self.ids.ura_file_name.text
+        ura_file_name = self.ids.ura_file_name.text
         context = self.ids.context_input.text
         ip = self.ids.ip_input.text
         path =self.ids.path_input.text
         custom_path = self.ids.custom_path_input.text
 
-        
-        # if self.ids.btn.text == 'Female':
-        #     gender = 'f'
-        # elif self.ids.btn.text == 'Male':
-        #     gender = 'm'
-        # else:
-        #     gender = 'f'
-
-        # if self.ids.check_debug.active:
-        #     debug = 1
-
-        gender, context, path, custom_path, debug = validations(gender, context, path, custom_path, debug)
+        gender, context, path, custom_path, debug, ip = validations(gender, context, path, custom_path, debug, ip)
 
         ura_header = header(context, ip, path, custom_path, gender)
 
-        with open("ura_file_name" + '.conf', 'w+') as f:
+        with open(str(ura_file_name) + '.conf', 'w+') as f:
             f.write(str(ura_header) + '\n')
-
 
 
 class UraApp(App):
